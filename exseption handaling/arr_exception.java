d1df6d2919ef7a7d7a10557a76fa0d7349a92a3b
jagannath <offical.jagannath1@gmail.com>
2026-09-03 12:15:17 +0530

    ne
    new

---
+++ exseption handaling/arr_exception.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+class arr_exception {
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+     int R, C;
+      while (true) { 
+        try {
+            System.out.print("enter value in  Row :");
+          R = sc.nextInt();
+          break;
+        } catch (Exception e) {
+            System.out.println("Invalid input. Please enter a valid integer.");
+            sc.nextLine(); // Clear the invalid input
+        }
+          
+      }
+      while (true) { 
+        try {
+            System.out.print("enter value in Column :");
+             C = sc.nextInt();
+             break;
+        } catch (Exception e) {
+            System.out.println("Invalid input. Please enter a valid integer.");
+            sc.nextLine(); // Clear the invalid input
+        }
+      }
+      
+      
+      int a[][] = new int[R][C];
+      for (int i = 0; i < R; i++) {
+
+         for (int j = 0; j < C; j++) {
+            while (true) { 
+                try {
+                    System.out.print(" enter value in Row ["+i+"]  Column ["+j+"] :");
+                    a[i][j]=sc.nextInt();
+                    break;
+                } catch (Exception e) {
+                    System.out.println("Invalid input. Please enter a valid integer.");
+                    sc.nextLine(); // Clear the invalid input
+                }
+            }
+
+         }
+            System.out.println("");
+      }
+      System.out.println("\nvalues in array are :");
+      for (int i = 0; i < R; i++) {
+         
+         for (int j = 0; j < C; j++) {
+            System.out.print("Row ["+i+"]  Column ["+j+"] :");
+            System.out.println(a[i][j]);
+         }
+         System.out.println("");
+      }
+      
+
+   }
+
+}
+ //System.out.println(a+"array ["+i+"]");
--- file handling/f1.java
+++ file handling/f1.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.io.FileWriter;
 
 class f1 {
     public static void main(String[] args) 
@@ -9,7 +10,10 @@ class f1 {
         if (f.createNewFile()) {
             System.out.println("File created: " + f.getName());
         } else {
-            System.out.println("File already exists.");
-        }
-    }
+            System.out.println("File already exists : ........." + f.getName()+".......");
+        }  
+        FileWriter n=new FileWriter(" j.txt");
+        n.write(" hii am  jp learnig java in tds institud"); 
+        n.close();
+    } 
 }
